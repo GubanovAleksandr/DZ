865ad9d0c2fd6df47ae28ecb51fb09f41b164cdd
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-22 23:27:11 +0500

    Задача 6 РЕШЕНО

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -2,3 +2,9 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+    Console.WriteLine("Введенное число является четным");
+else
+    Console.WriteLine("Введенное число нечетно");
