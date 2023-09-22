7a11d4a710b8fc35e27580c1298c0a0eaec035c4
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-22 23:52:02 +0500

    Задача 8 Решено

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -6,7 +6,7 @@ int number = Convert.ToInt32(Console.ReadLine());
 if (number < 0) number *= -1;
 if (number > 0)
 {
-    int count = 0;
+    int count = 2;
     while (count <= number)
     {
        Console.Write($"{count}, ");
