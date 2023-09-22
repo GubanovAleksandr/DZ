72efba24cdef92d5a17501cb9e1f2cf5002d60aa
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-22 21:27:54 +0500

    Задача 7 - work

--- Task007/Program.cs
+++ Task007/Program.cs
@@ -1,2 +1,5 @@
-﻿Console.WriteLine("Введите целое положительное число");
+﻿Console.WriteLine("Введите трехзначное целое число");
 int number = Convert.ToInt32(Console.ReadLine());
+
+int lastDiget = number%10;
+Console.WriteLine($"последняя цифра числа: {lastDiget}");
