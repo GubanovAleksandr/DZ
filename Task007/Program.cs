fe5357dc7ab3018d4003580640d653d191747ac5
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-22 21:34:33 +0500

    Задача 7 - work

--- Task007/Program.cs
+++ Task007/Program.cs
@@ -1,5 +1,10 @@
 ﻿Console.WriteLine("Введите трехзначное целое число");
 int number = Convert.ToInt32(Console.ReadLine());
-
-int lastDiget = number%10;
-Console.WriteLine($"последняя цифра числа: {lastDiget}");
+if (number < 0) number *= -1;
+if (number >= 100 && number <= 999)
+{
+    int lastDiget = number % 10;
+    Console.WriteLine($"последняя цифра числа: {lastDiget}");
+}
+else
+    Console.WriteLine("Неверное число");
