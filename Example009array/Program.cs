2f42c8d4e82ffb949ced65d29651aaa7f6881c95
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-26 10:32:02 +0500

    nassiv

--- Example009array/Program.cs
+++ Example009array/Program.cs
@@ -5,10 +5,13 @@
     if (arg3 > result) result = arg3;
     return result;
 }//             0  1   2   3    4   5   6   7   8
-int[] array = {1, 27, 7, 488, 588, 68, 7, 88, 988};
-array[0] = 12;
-Console.WriteLine(array[4]);
+int[] array = {1, 27, 7, 488, 588, 68, 7777, 88, 988};
+//array[0] = 12;
+//Console.WriteLine(array[4]);
 
-//int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8]));
 
-//Console.WriteLine(max);
+Console.WriteLine(max);
