28442f0e51d0bd4e4a7c9bce2008ae524e604d6b
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-26 11:54:01 +0500

    Print arra

--- Exampl011/Program.cs
+++ Exampl011/Program.cs
@@ -1,26 +1,46 @@
-﻿void FillArray(int []collection)
+﻿void FillArray(int[] collection)
 {
     int length = collection.Length;
-    int index =0;
-    while(index<length)
+    int index = 0;
+    while (index < length)
     {
-        collection[index]=new Random().Next(1,10);
-        index=index +1;
+        collection[index] = new Random().Next(1, 10);
+        index = index + 1;
     }
 }
 
-void PrintArray(int[]col)
+void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
-    while(position<count)
+    while (position < count)
     {
         Console.WriteLine(col[position]);
         position++;
-         }
+    }
 }
 
-int [] array = new int [10];
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = Index0f(array, 4);
+Console.WriteLine(pos);
