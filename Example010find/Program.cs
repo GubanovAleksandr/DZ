0d9a9371cbe17be0dcc9198006fa24ab6ae7cddc
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-26 11:01:20 +0500

    find

---
+++ Example010find/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 12, 13, 2, 3, 4, 555, 56, 77, };
+int n = array.Length;
+int find = 12;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;
+    //index= index +1;
+    }
+}
+Console.WriteLine(index);
