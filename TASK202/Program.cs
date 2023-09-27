fcfd7bad7e2640b370437e9c2b99958ba20c9bd3
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-27 10:16:10 +0500

    программа удаления второго числа

---
+++ TASK202/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100, 1000);
+//Console.WriteLine($"{number}- > {numResult}");
+
+int deliteSecondDigit = DeliteSecondDigit(number);
+Console.WriteLine($"{number}- > {deliteSecondDigit}");
+
+int DeliteSecondDigit(int num)
+{
+    int firstDigit = num /100;
+    int lastDigit = num % 10;
+    int numResult = firstDigit*10+lastDigit;
+    return numResult;
+}
