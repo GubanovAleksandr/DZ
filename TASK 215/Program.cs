266ab74c25bd0685e0c66fa2f0411fcb5ae93acb
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-27 12:21:08 +0500

    решение 214

---
+++ TASK 215/Program.cs
@@ -0,0 +1,11 @@
+﻿bool IsOneSquareOfTheOther(int num1, int num2)
+{
+    return num1 * num1 == num2 ||num2 * num2 == num1;
+ }
+Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsOneSquareOfTheOther(number1, number2);
+Console.WriteLine(result ? "да" : "нет");
---
+++ TASK214/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+bool Multiple0fTwoNumbers(int num)
+{
+    bool multipal = (num % 7 == 0) && (num % 23 == 0);
+    return multipal;
+
+}
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = Multiple0fTwoNumbers(number);
+Console.WriteLine(result ? "да" : "нет");
