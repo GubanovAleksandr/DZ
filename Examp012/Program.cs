2b1b2cb94d6ebc407aafac99e630ff3b158eadbb
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-27 10:14:32 +0500

    1

--- Examp012/Program.cs
+++ Examp012/Program.cs
@@ -3,11 +3,25 @@
 
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка от 10, 99 ->{number}");
-int firstDigit = number / 10;  //78/10 =7.8 - > так как инт то получается "7"
-int secondDigit = number % 10;
+//int firstDigit = number / 10;  //78/10 =7.8 - > так как инт то получается "7"
+//int secondDigit = number % 10;
 
-if (firstDiget > secondDigit Digit) Console.WriteLine($"Наибольшая цифра числа {FirstDiget}");
-else Console.WriteLine($"Наибольшеая цифра числа ->"{ secondDigit});
+//if (firstDiget > secondDigit Digit) Console.WriteLine($"Наибольшая цифра числа {FirstDiget}");
+//else Console.WriteLine($"Наибольшеая цифра числа ->"{ secondDigit});
 //тернарный оператор
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"Наибольшеая цифра числа ->"{ maxDigit});
+//int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+//Console.WriteLine($"Наибольшеая цифра числа ->"{ maxDigit});
+
+
+//заменяем программу методом 
+int maxDigit = MaxDigit(number);
+Console.WriteLine(maxDigit);
+
+int MaxDigit(int num)
+{
+    int firstDiget = num / 10 ;
+    int secondDigit = num % 10;
+    return firstDiget > secondDigit ? firstDiget : secondDigit;
+
+}
+//
