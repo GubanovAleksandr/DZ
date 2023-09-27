40ec476b5868ef48200725f9e3e115f2816f33d7
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-27 10:33:12 +0500

    проверка кратности на число

---
+++ TASK203/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int Remainder(int firstPar, int secondPar)
+{
+    return firstPar % secondPar;
+}
+int remainder = Remainder(firstNumber, secondNumber);
+if (remainder == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"не Кратно, остаток{remainder}");
