9587e68a75ccc94507e6228b7aa3cd33de099689
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-28 11:17:49 +0500

    Архив заданий

---
+++ TASK17/Program.cs
@@ -0,0 +1,24 @@
+﻿// программа выдает номре четверти
+
+int Quater(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
+Console.WriteLine("Введите координаты точки");
+Console.WriteLine("X:");
+int xCoordinate  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Y:");
+int yCoordinate  = Convert.ToInt32(Console.ReadLine());
+
+int quater= Quater(xCoordinate, yCoordinate);
+
+string result = quater >0
+                ? $"Указананные координаты соответвуют четверти {quater}"
+                : "Введены некорректные координаты";
+
+Console.WriteLine(result);
